980bf0725b39739c92fc3961268cd50ac5950b13
Amdrii <andreyk557@gmail.com>
2023-03-26 14:17:36 +0300

    feat[lastPoint/func]

--- app.js
+++ app.js
@@ -49,22 +49,22 @@ function Particle(x, y, radius, color) {
   this.color = color;
   this.radians = Math.random() * Math.PI * 2;
   this.velocity = 0.05;
-  this.distanceFromCenter = {
-    x: randomIntFromRange(50, 120),
-    y: randomIntFromRange(50, 120),
-  };
+  this.distanceFromCenter = randomIntFromRange(50, 120);
 
   this.update = () => {
+    const lastPoint = { x: this.x, y: this.y };
     this.radians += this.velocity;
-    this.x = x + Math.cos(this.radians) * this.distanceFromCenter.x;
-    this.y = y + Math.sin(this.radians) * this.distanceFromCenter.y;
-    this.draw();
+    this.x = x + Math.cos(this.radians) * this.distanceFromCenter;
+    this.y = y + Math.sin(this.radians) * this.distanceFromCenter;
+    this.draw(lastPoint);
   };
-  this.draw = () => {
+  this.draw = (lastPoint) => {
     c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
+    c.strokeStyle = this.color;
+    c.lineWidth = this.radius;
+    c.moveTo(lastPoint.x, lastPoint.y);
+    c.lineTo(this.x, this.y);
+    c.stroke();
     c.closePath();
   };
 }
@@ -85,7 +85,8 @@ function init() {
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = "rgba(255,255,255, 0.05)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
   particles.forEach((particle) => {
     particle.update();
